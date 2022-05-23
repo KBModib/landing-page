34d38f849f18a334736dae253280f3f04a086260
KBModib <kmodibedi@outlook.com>
2022-05-23 15:02:09 +0200

    update

--- index.js
+++ index.js
@@ -86,27 +86,3 @@ function LoadName () {
     document.getElementById("profile-text").innerHTML = localStorage.getItem("fname");
     document.getElementById("profile-pic").src = localStorage.getItem("image");
 }
-
-//js for scroller 
-function ScrollBooks(side){
-    const elem = document.getElementById("horizontal-scroll");
-    let scroller = elem.scrollLeft;
-    let elemWidth = elem.clientWidth;
-    let scrollableWidth = elem.scrollWidth;
-    let total = scrollableWidth - scroller - elemWidth;
-
-    if( side == "left")
-    {
-        if(total != elem.offsetWidth)
-        {
-            elem.scrollLeft += 200;
-        }
-    }
-    else{
-        if( elem.scrollLeft != 0)
-        {
-            elem.scrollLeft -= 200;
-        }
-    }
-    
-}
