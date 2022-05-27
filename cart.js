19c84c85267acff5d31c94a0fcab857675761d84
KBModib <kmodibedi@outlook.com>
2022-05-27 16:41:38 +0200

    fri update

--- cart.js
+++ cart.js
@@ -91,12 +91,12 @@ function updateCartTotal() {
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total = 0
     for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('R', ''))
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        var price = parseFloat(priceElement.innerText.replace('Price:R', ''));
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
     }
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = 'R' + total
